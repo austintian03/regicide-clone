d72c108d775721e43a99069de096573dee5286ee
Austin Tian <47795058+austintian03@users.noreply.github.com>
2025-05-19 11:34:20 -0400

    Fix unused _delta warning

--- scripts/card_ui.gd
+++ scripts/card_ui.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 	enter_state(State.BASE)
 
 # state transition conditions
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_pressed("right_mouse") and current_state == State.CLICKED:
 		switch_state(State.BASE)
 
