cd34b7387411a6d11a0f9633759535ce2f62da84
Austin Tian <47795058+austintian03@users.noreply.github.com>
2025-05-20 17:35:56 -0400

    Create CardPile resource that holds CardResource objects in an array

---
+++ custom_resources/card_pile.gd
@@ -0,0 +1,35 @@
+class_name CardPile
+extends Resource
+
+signal card_pile_size_changed(cards_amount: int)
+
+var cards : Array[CardResource] = []
+var id : String
+
+func _init(p_id: String) -> void:
+	id = p_id
+	
+func empty() -> bool:
+	return cards.is_empty()
+
+func draw_card() -> CardResource:
+	var card = cards.pop_front()
+	card_pile_size_changed.emit(cards.size())
+	return card
+
+func add_card(card: CardResource) -> void:
+	cards.append(card)
+	card_pile_size_changed.emit(cards.size())
+
+func shuffle() -> void:
+	cards.shuffle()
+
+func clear() -> void:
+	cards.clear()
+
+func _to_string() -> String:
+	var _pile_name = id + "\n" 
+	var _card_strings: PackedStringArray = []
+	for i in range(cards.size()):
+		_card_strings.append("%s: %s of %s" % [i+1, cards[i].rank, cards[i].Suit.keys()[cards[i].suit]])
+	return  _pile_name + "\n".join(_card_strings)
