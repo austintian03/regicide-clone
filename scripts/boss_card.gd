9abc72fd3450df7101db7b4f762aa2eb6210312c
Austin Tian <adt339@nyu.edu>
2025-05-23 19:39:52 -0400

    Implement initial version of BossCard class

---
+++ scripts/boss_card.gd
@@ -0,0 +1,31 @@
+class_name BossCard
+extends Node2D
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+var card: CardResource
+var health: int
+var attack: int
+
+func set_card(p_card: CardResource) -> void:
+	card = p_card
+	sprite.texture = card.texture
+	match card.rank:
+		11:
+			health = 20
+			attack = 10
+		12:
+			health = 30
+			attack = 15
+		13:
+			health = 40
+			attack = 20
+
+func take_damage(dmg: int) -> void:
+	health -= dmg
+
+func do_damage() -> int:
+	return attack
+
+func on_death() -> CardResource:
+	return self.card
