674f201dc090a5fb313f29a69c6509d4da35b539
Austin Tian <adt339@nyu.edu>
2025-05-23 19:42:29 -0400

    Handle initialization of Enemies pile and BossCard

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var hand: HandUI = $CanvasLayer/HandUI
+@onready var boss_card: BossCard = $BossCard
+
 var deck := CardPile.new("Deck")
 var enemies := CardPile.new("Enemies")
 var discard := CardPile.new("Discard")
@@ -13,17 +15,26 @@ const ROYALS = [11, 12, 13]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# populate deck resource with cards and draw cards into Hand
-	populate_deck()
+	# populate player deck with cards and draw starting hand
+	populate(deck, NUMS)
 	fill_hand()
+	# populate enemy pile with the royals
+	populate_enemies()
+	boss_card.set_card(enemies.draw_card())
 
-func populate_deck() -> void:
+func populate(pile: CardPile, ranks: Array) -> void:
 	for s in SUITS:
-		for r in NUMS:
-			deck.add_card(CardResource.new(s, r))
-	deck.shuffle()
-	print(deck)
+		for r in ranks:
+			pile.add_card(CardResource.new(s, r))
+	pile.shuffle()
+	print(pile)
 
+func populate_enemies() -> void:
+	var temp_pile = CardPile.new("temp")
+	for r in ROYALS:
+		populate(temp_pile, [r])
+		enemies.add_cards(temp_pile.draw_cards(temp_pile.get_size()))
+		
 func fill_hand(num_cards: int = 8) -> void:
 	var hand_size = hand.hand_size()
 	if hand_size < MAX_HAND_SIZE:
