ea4d5ad86c425042c96ead838f8385620c440df9
Austin Tian <adt339@nyu.edu>
2025-06-01 00:24:46 -0400

    Implement card effect resolution and initial game logic

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,23 +3,36 @@ extends Node2D
 @onready var hand: HandUI = $CanvasLayer/HandUI
 @onready var boss_card: BossCard = $BossCard
 
+# relevant card piles
 var deck := CardPile.new("Deck")
 var enemies := CardPile.new("Enemies")
 var discard := CardPile.new("Discard")
 
+# relevant player information
+var player_damage: int = 0
+var player_defense: int = 0
+
+# constants
 const MAX_HAND_SIZE = 8
-const SUITS = ["clubs", "diamonds", "hearts", "spades"]
+const SUITS = ["clubs", "hearts", "spades", "diamonds"]
 const NUMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const ROYALS = [11, 12, 13]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Events.connect("cards_played", _on_cards_played)
 	# populate player deck with cards and draw starting hand
 	populate(deck, NUMS)
 	fill_hand()
+
 	# populate enemy pile with the royals
 	populate_enemies()
 	boss_card.set_card(enemies.draw_card())
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
 
 func populate(pile: CardPile, ranks: Array) -> void:
 	for s in SUITS:
@@ -33,7 +46,7 @@ func populate_enemies() -> void:
 	for r in ROYALS:
 		populate(temp_pile, [r])
 		enemies.add_cards(temp_pile.draw_cards(temp_pile.get_size()))
-		
+
 func fill_hand(num_cards: int = 8) -> void:
 	var max_cards_can_draw = MAX_HAND_SIZE - hand.hand_size()
 	var cards_to_draw = min(num_cards, max_cards_can_draw)
@@ -41,6 +54,38 @@ func fill_hand(num_cards: int = 8) -> void:
 		hand.add_and_sort_card(deck.draw_card())
 	print(hand)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _on_cards_played(played_cards: Array[CardResource]) -> void:
+	# resolve card effects, deal damage to boss card
+	process_play_effects(played_cards)
+	boss_card.take_damage(player_damage)
+	
+	# if boss card still alive, resolve boss attack
+	var post_mitigation_dmg = max(boss_card.do_damage() - player_defense, 0)
+	Events.emit_signal("boss_card_attacking", post_mitigation_dmg)
+
+func process_play_effects(played_cards: Array[CardResource]) -> void:
+	# set player_damage to sum of played cards
+	var card_sum = played_cards.reduce(func(accum, card): return accum + card.rank, 0)
+	player_damage = card_sum
+	
+	# resolve suit effects of played cards (boss suit negates matching suit effect)
+	var card_suits = played_cards.map(func(card): return SUITS[card.suit])
+	for s in SUITS:
+		if s in card_suits and s != SUITS[boss_card.card.suit]:
+			resolve_suit_effect(s, card_sum)
+
+func resolve_suit_effect(suit: String, val: int) -> void:
+	match suit:
+		# double damage
+		"clubs": 
+			player_damage *= 2
+		# draw cards to hand
+		"diamonds":
+			fill_hand(val)
+		# restore random cards from discard to bottom of deck
+		"hearts":
+			discard.shuffle()
+			deck.add_cards(discard.draw_cards(val))
+		# add defense
+		"spades":
+			player_defense += val
