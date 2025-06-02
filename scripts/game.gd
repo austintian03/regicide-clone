99af26b1ea8b2ef55473492e10ef526d1182a0a4
Austin Tian <47795058+austintian03@users.noreply.github.com>
2025-06-01 23:06:00 -0400

    Implement game logic for boss turns and add debug print statements

--- scripts/game.gd
+++ scripts/game.gd
@@ -20,11 +20,15 @@ const ROYALS = [11, 12, 13]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# connect event signals that trigger game effects
 	Events.connect("cards_played", _on_cards_played)
+	Events.connect("boss_card_damaged", _on_boss_card_damaged)
+	
 	# populate player deck with cards and draw starting hand
 	populate(deck, NUMS)
 	fill_hand()
-
+	print("Starting hand: ", hand)
+	
 	# populate enemy pile with the royals
 	populate_enemies()
 	boss_card.set_card(enemies.draw_card())
@@ -32,7 +36,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
 
 func populate(pile: CardPile, ranks: Array) -> void:
 	for s in SUITS:
@@ -52,16 +55,46 @@ func fill_hand(num_cards: int = 8) -> void:
 	var cards_to_draw = min(num_cards, max_cards_can_draw)
 	for i in range(cards_to_draw):
 		hand.add_and_sort_card(deck.draw_card())
-	print(hand)
+
+# connected signal events which trigger game effects
+func _on_boss_card_damaged(boss_health: int) -> void:
+	print("Resolving boss turn")
+	resolve_boss_turn(boss_health)
 
 func _on_cards_played(played_cards: Array[CardResource]) -> void:
+	print("Resolving player turn")
 	# resolve card effects, deal damage to boss card
+	resolve_player_turn(played_cards)
+
+# game effect functions
+func resolve_player_turn(played_cards: Array[CardResource]) -> void:
 	process_play_effects(played_cards)
+	print("Attacking boss for ", player_damage)
 	boss_card.take_damage(player_damage)
+
+func resolve_boss_turn(boss_health: int) -> void:
+	if boss_health <= 0:
+		process_boss_death(boss_health)
+	else:
+		print("Boss card attacking!")
+		var post_mitigation_dmg = max(boss_card.do_damage() - player_defense, 0)
+		Events.emit_signal("boss_card_attacking", post_mitigation_dmg, player_defense)
+
+func process_boss_death(boss_health: int) -> void:
+	# release card resource to the correct pile depending on perfect lethal or not
+	if boss_health == 0:
+		print("Perfect lethal!")
+		deck.add_card_on_top(boss_card.on_death())
+	elif boss_health < 0:
+		print("Royalty vanquished.")
+		discard.add_card(boss_card.on_death())
 	
-	# if boss card still alive, resolve boss attack
-	var post_mitigation_dmg = max(boss_card.do_damage() - player_defense, 0)
-	Events.emit_signal("boss_card_attacking", post_mitigation_dmg)
+	# draw new boss card or win game
+	if enemies.get_size() > 0:
+		boss_card.set_card(enemies.draw_card())
+	else:
+		boss_card.queue_free()
+		print("You win!")
 
 func process_play_effects(played_cards: Array[CardResource]) -> void:
 	# set player_damage to sum of played cards
@@ -79,13 +112,17 @@ func resolve_suit_effect(suit: String, val: int) -> void:
 		# double damage
 		"clubs": 
 			player_damage *= 2
+			print("Doubled damage to ", player_damage)
 		# draw cards to hand
 		"diamonds":
+			print("Drawing up to ", val)
 			fill_hand(val)
 		# restore random cards from discard to bottom of deck
 		"hearts":
+			print("Restoring up to ", val, " cards back to Tavern deck")
 			discard.shuffle()
 			deck.add_cards(discard.draw_cards(val))
 		# add defense
 		"spades":
 			player_defense += val
+			print("Defense is now ", player_defense)
