29e3761da83af3a2175c301d8b377205e6bcffda
Austin Tian <47795058+austintian03@users.noreply.github.com>
2025-06-02 20:44:01 -0400

    Implement Play/Discard phase sequence and reorganize functions

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var hand: HandUI = $CanvasLayer/HandUI
 @onready var boss_card: BossCard = $BossCard
+@onready var play_button: Button = $CanvasLayer/PlayButton
+@onready var discard_button: Button = $CanvasLayer/DiscardButton
 
 # relevant card piles
 var deck := CardPile.new("Deck")
@@ -24,6 +26,7 @@ func _ready() -> void:
 	# connect event signals that trigger game effects
 	Events.connect("cards_played", _on_cards_played)
 	Events.connect("boss_card_damaged", _on_boss_card_damaged)
+	Events.connect("cards_discarded", _on_cards_discarded)
 	
 	# populate player deck with cards and draw starting hand
 	populate(deck, NUMS)
@@ -34,52 +37,40 @@ func _ready() -> void:
 	populate_enemies()
 	boss_card.set_card(enemies.draw_card())
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-func populate(pile: CardPile, ranks: Array) -> void:
-	for s in SUITS:
-		for r in ranks:
-			pile.add_card(CardResource.new(s, r))
-	pile.shuffle()
-	#print(pile)
-
-func populate_enemies() -> void:
-	var temp_pile = CardPile.new("temp")
-	for r in ROYALS:
-		populate(temp_pile, [r])
-		enemies.add_cards(temp_pile.draw_cards(temp_pile.get_size()))
-
-func fill_hand(num_cards: int = 8) -> void:
-	var max_cards_can_draw = MAX_HAND_SIZE - hand.hand_size()
-	var cards_to_draw = min(num_cards, max_cards_can_draw)
-	for i in range(cards_to_draw):
-		hand.add_and_sort_card(deck.draw_card())
-
-# connected signal events which trigger game effects
+# connected event signals which trigger game effects
+# these signals progress the game state in the order they appear in
+func _on_cards_played(played_cards: Array[CardResource]) -> void:
+	print("Resolving player turn")
+	resolve_player_turn(played_cards)
+	
 func _on_boss_card_damaged(boss_health: int) -> void:
 	print("Resolving boss turn")
 	resolve_boss_turn(boss_health)
-
-func _on_cards_played(played_cards: Array[CardResource]) -> void:
-	print("Resolving player turn")
-	# resolve card effects, deal damage to boss card
-	resolve_player_turn(played_cards)
+	
+func _on_cards_discarded(discarded_cards: Array[CardResource]) -> void:
+	discard.add_cards(discarded_cards)
+	swap_buttons()
 
 # game effect functions
+# these process game logic/rules using the values passed alongside the emitted game event signals
 func resolve_player_turn(played_cards: Array[CardResource]) -> void:
+	# resolve card effects, deal damage to boss card
 	process_play_effects(played_cards)
 	print("Attacking boss for ", player_damage)
 	boss_card.take_damage(player_damage)
 
 func resolve_boss_turn(boss_health: int) -> void:
+	# process boss death or attack player
 	if boss_health <= 0:
 		process_boss_death(boss_health)
 	else:
+		# calc damage player is to take
 		var post_mitigation_dmg = max(boss_card.do_damage() - player_defense, 0)
 		print("Boss card attacking for ", post_mitigation_dmg)
-		Events.emit_signal("boss_card_attacking", post_mitigation_dmg, player_defense)
+		# only go to discard phase if player is taking damage
+		if post_mitigation_dmg > 0:
+			swap_buttons()
+			hand.discard_target = post_mitigation_dmg
 
 func process_boss_death(boss_health: int) -> void:
 	# reset player defense
@@ -97,7 +88,7 @@ func process_boss_death(boss_health: int) -> void:
 		discard.add_card(boss_card.on_death())
 	
 	# draw new boss card or win game
-	if enemies.is_empty():
+	if !enemies.is_empty():
 		boss_card.set_card(enemies.draw_card())
 	else:
 		boss_card.queue_free()
@@ -135,3 +126,31 @@ func resolve_suit_effect(suit: String, val: int) -> void:
 		"spades":
 			player_defense += val
 			print("Defense is now ", player_defense)
+
+# utility functions
+func fill_hand(num_cards: int = 8) -> void:
+	var max_cards_can_draw = MAX_HAND_SIZE - hand.hand_size()
+	var cards_to_draw = min(num_cards, max_cards_can_draw)
+	for i in range(cards_to_draw):
+		hand.add_and_sort_card(deck.draw_card())
+
+func swap_buttons() -> void:
+	var temp_disabled = play_button.disabled
+	var temp_visible = play_button.visible
+	play_button.disabled = discard_button.disabled
+	play_button.visible = discard_button.visible
+	discard_button.disabled = temp_disabled
+	discard_button.visible = temp_visible
+
+func populate(pile: CardPile, ranks: Array) -> void:
+	for s in SUITS:
+		for r in ranks:
+			pile.add_card(CardResource.new(s, r))
+	pile.shuffle()
+	#print(pile)
+
+func populate_enemies() -> void:
+	var temp_pile = CardPile.new("temp")
+	for r in ROYALS:
+		populate(temp_pile, [r])
+		enemies.add_cards(temp_pile.draw_cards(temp_pile.get_size()))
